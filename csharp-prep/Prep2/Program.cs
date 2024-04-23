961af70ed626fa5849b5f91fd67afa4a285930e0
chapman-jackson <jacksie17@byui.edu>
2024-04-23 17:40:20 -0600

    Completed program two

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
@@ -8,64 +9,75 @@ class Program
         string number = Console.ReadLine();
         int grade = int.Parse(number);
 
+        string letter = "";
         if (grade >= 90)
         {
-            Console.WriteLine("Your grade is an A. You passed the class!");
+            letter ="A";
         }
 
         else if (grade >= 90 && grade < 94)
         {
-            Console.WriteLine("Your grade is an A-. You passed the class!");
+            letter = "A-";
         }
 
         else if (grade >= 87 && grade < 90)
         {
-            Console.WriteLine("Your grade is a B+. You passed the class!");
+            letter = "B+";
         }
 
         else if (grade >= 80 && grade < 84)
         {
-            Console.WriteLine("Your grade is a B-. You passed the class!");
+            letter = "B-";
         }
 
         else if (grade >= 80 && grade < 90)
         {
-            Console.WriteLine("Your grade is a B. You passed the class!");
+            letter = "B";
         }
 
         else if (grade >= 77 && grade < 80)
         {
-            Console.WriteLine("Your grade is a C+. You passed the class!");
+            letter = "C+";
         }
 
         else if (grade >= 70 && grade < 74)
         {
-            Console.WriteLine("Your grade is a C-. You passed the class!");
+            letter = "C-";
         }
 
         else if (grade >= 70 && grade < 80)
         {
-            Console.WriteLine("Your grade is a C. You passed the class!");
+            letter = "C";
         }
 
         else if (grade >= 67 && grade < 70)
         {
-            Console.WriteLine("Your grade is a D+. You did not pass the class.");
+           letter = "D+"; 
         }
 
          else if (grade >= 60 && grade < 64)
         {
-            Console.WriteLine("Your grade is a D-. You did not pass the class.");
+            letter = "D-";
         }
 
          else if (grade >= 60 && grade < 70)
         {
-            Console.WriteLine("Your grade is a D. You did not pass the class.");
+            letter = "D";
         }
 
         else if (grade < 60)
         {
-            Console.WriteLine("Your grade is a F. You did not pass the class.");
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Your grade is a {letter}. You passed the class!");
+        }
+
+        else
+        {
+            Console.WriteLine($"Your grade is a {letter}. You did not pass the class. I know you can do it next time.");
         }
     }
 }
