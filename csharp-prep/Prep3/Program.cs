e580d4108ec68812e971eb23e2884fa68aa6210b
chapman-jackson <jacksie17@byui.edu>
2024-04-24 09:59:53 -0600

    Completed program three.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        int guess = 9999999;
+
+        int counter = 0;
+
+        while (guess != number)
+        {
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    counter = counter +1;
+                    Console.WriteLine("Correct!");
+                    Console.WriteLine($"It took you {counter} attempts to guess correctly.");
+                }
+
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                    counter = counter +1;
+                }
+
+                 else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                    counter = counter + 1;
+                }
+        }
     }
 }
