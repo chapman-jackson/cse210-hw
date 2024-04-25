b7678bd7fd4ce3c9f24e83240b1b972d7bb9033a
chapman-jackson <jacksie17@byui.edu>
2024-04-24 23:01:43 -0600

    Completed program four.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = 3;
+
+        while (number != 0)
+        {
+            Console.WriteLine("Enter number:");
+
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        
+        int total = 0;
+        foreach (int digit in numbers)
+        {
+            total += digit;
+        }
+
+
+        Console.WriteLine($"Sum: {total}");
+
+        float average_num = ((float)total)/ numbers.Count;
+
+        Console.WriteLine($"Average: {average_num}");
+
+        int maximum = numbers[0];
+
+        foreach (int digits in numbers)
+        {
+            if (digits > maximum)
+            {
+                maximum = digits;
+            }
+        }
+
+        Console.WriteLine($"Highest number: {maximum}");
     }
 }
