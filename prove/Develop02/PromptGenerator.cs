456f1420c5e44bc1103f9c4d731f248b845d91ad
chapman-jackson <jacksie17@byui.edu>
2024-05-02 17:31:02 -0600

    attempt at Promptgenerator file

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -4,7 +4,15 @@ public class PromptGenerator
 
       public List<string> _prompts = new List<string>();
 
-        
+        _prompt.Add("What was your favorite part about today?");
+
+        _prompt.Add("What did you learn about in your gospel study?");
+
+        _prompt.Add("What interesting conversation did you have with someone?");
+
+        _prompt.Add("What did you enjoy seeing today?");
+
+        _prompt.Add("If there is one thing you could improve, what would it be?");
 
         public string GetRandomPrompt()
         {
