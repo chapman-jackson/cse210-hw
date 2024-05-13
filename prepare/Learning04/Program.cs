078dcdf50bc30ca20f38dca086bc2184e75931d0
chapman-jackson <156723703+chapman-jackson@users.noreply.github.com>
2024-05-13 13:56:52 -0600

    Add files via upload
    Completed week four learning.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
+class Program 
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Bob Marley","Calculus");
+        string homework = assignment.GetSummary();
+        Console.WriteLine(homework);
+        Console.WriteLine();
+        
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Section 7.3", "Fractions", "Problems 8-19");
+        string homeworkOne = mathAssignment.GetHomeworkList();
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(homeworkOne);
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters","The causes of World War II","European History");
+        string homeworkTwo = writingAssignment.GetWritingInformation();
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(homeworkTwo);
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,14 @@
+public class WritingAssignment : Assignment
+{ 
+    private string _title;
+    
+    public WritingAssignment(string studentName, string title, string topic) : base(studentName, topic)
+    {
+        _title = title;
+    }
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+        return $"{_title} by {studentName}";
+    }
+}
