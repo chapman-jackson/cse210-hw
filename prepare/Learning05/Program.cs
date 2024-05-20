3175decb8b92f553023cad2c650b7497dc91fccb
chapman-jackson <156723703+chapman-jackson@users.noreply.github.com>
2024-05-20 16:50:12 -0600

    Completed week five learning.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shape = new List<Shape>();
+
+        Square c1 = new Square("green",6);
+        shape.Add(c1);
+
+        Circle c2 = new Circle("pink",3);
+        shape.Add(c2);
+
+        Rectangle c3 = new Rectangle("black",4,12);
+        shape.Add(c3);
+
+        foreach (Shape c in shape)
+        {
+            string color = c.GetColor();
+
+            double area = c.GetArea();
+
+            Console.WriteLine($"The {color} colored shape has the area {area}.");
+        }
+
     }
 }
